112e3c915074fcb5de927eb7fee3568689f37323
M4tuR <matvei.raud@voco.ee>
2023-11-24 15:28:10 +0200

    YL2:Kustutab kasutaja

---
+++ yl2.ps1
@@ -0,0 +1,11 @@
+﻿$eesnimi = Read-Host "Sisesta enda eesnimi"
+$perenimi = Read-Host "Sisesta oma perenimi"
+$kasutajanimi = "$eesnimi.$perenimi".ToLower()
+Write-Host "Kustutatav kasutaja on" $kasutajanimi
+Try {
+Remove-LocalUser -Name $kasutajanimi -ErrorAction Stop
+Write-Host "Kasutaja kustutamine õnnestus."
+}
+Catch {
+Write-Host "Kasutaja kustutamine ebaõnnestus"
+}
