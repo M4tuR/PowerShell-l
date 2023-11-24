5295d60631be65be6ca5db809abd56ee02cb05d3
M4tuR <matvei.raud@voco.ee>
2023-11-24 13:56:14 +0200

    Yl1:Loob kasutaja

---
+++ yl1.ps1
@@ -0,0 +1,13 @@
+﻿$eesnimi = Read-Host "Sisesta enda eesnimi"
+$perenimi = Read-Host "Sisesta oma perenimi"
+$kasutajanimi = "$eesnimi.$perenimi".ToLower()
+Write-Host "Loodav kasutaja on" $kasutajanimi
+$parool = "Parool1!"
+$secureString = ConvertTo-SecureString $parool -AsPlainText -Force
+$params =@{
+Name = $kasutajanimi
+Password = $secureString
+FullName = "$eesnimi $perenimi"
+Description = "Local Acount - $kasutajanimi"
+}
+New-LocalUser @params
