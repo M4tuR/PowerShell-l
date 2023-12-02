60d43ec787349c964c36bb29c9cbfd51c15387ef
Matu <matvei.raud@voco.ee>
2023-12-02 19:46:59 +0200

    YL6.1:Teeb kõikide kasutajate kodukaustade backup failid

---
+++ yl6.1.ps1
@@ -0,0 +1,16 @@
+﻿$backupDirectory = "C:\Backup"
+
+# Loob varunduste jaoks kataloogi, kui see ei eksisteeri
+if (-not (Test-Path $backupDirectory)) {
+    New-Item -ItemType Directory -Path $backupDirectory | Out-Null
+}
+
+# Varunda kõikide kasutajate kodukaustad
+Get-ChildItem -Path C:\Users -Directory | ForEach-Object {
+    $username = $_.Name
+    $backupFileName = "$backupDirectory\$username-$(Get-Date -Format 'dd.MM.yyyy').zip"
+    
+    # Varunda kodukaust zip-failiks
+    Compress-Archive -Path $_.FullName -DestinationPath $backupFileName
+    Write-Host "Varundatud kasutaja: $username"
+}
