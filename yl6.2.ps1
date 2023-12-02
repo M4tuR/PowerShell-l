fb43fb3ea9e5204b6725a70d331f6efaad25dea5
Matu <matvei.raud@voco.ee>
2023-12-02 19:49:15 +0200

    YL6.2:Varundab sisestatud kasutaja kodukausta failid

---
+++ yl6.2.ps1
@@ -0,0 +1,16 @@
+﻿$eesnimi = Read-Host "Sisesta enda eesnimi"
+$perenimi = Read-Host "Sisesta oma perenimi"
+$kasutajanimi = "$eesnimi.$perenimi".ToLower()
+Write-Host "Varundatav kasutaja on" $kasutajanimi
+$testUser = $kasutajanimi
+$testUserBackupFile = "$backupDirectory\$testUser-$(Get-Date -Format 'dd.MM.yyyy').zip"
+$juurkataloog = "C:\Users\$testUser"
+# Kui varundusfail eksisteerib
+if (Test-Path $testUserBackupFile) {
+    # Pakib lahti varunduse
+    Expand-Archive -Path $testUserBackupFile -DestinationPath "C:\Users" -Force
+    Set-ADUser -Identity $testUser -HomeDirectory $juurkataloog -HomeDrive "C:"
+    Write-Host "Varunduse lahtipakkimine lõpetatud."
+} else {
+    Write-Host "Varundust ei leitud kasutajale: $testUser"
+}
